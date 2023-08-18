2a88e1e236f9f3fe26af506e018eb8705d6234b9
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-18 13:11:58 +0330

    feat: RegisterView added to account.views file

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    
+    def post(self, request):
+        serializer = CustomUserSerializer(request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(data=serializer.validated_data, status=status.HTTP_201_CREATED)
