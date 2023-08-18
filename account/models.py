92c2bd97095bb74ad6b13276db60d0a3f120e153
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-18 11:20:38 +0330

    feat: CustomUser added to account.models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email address")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.email
