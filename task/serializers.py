13e98f5b3ec0282dc8cc9606d9f0b1afdde17714
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-19 19:44:27 +0330

    fix: assigned_user removed from fields of TaskCreateSerializer. id field added to fields of TaskReadSerializer class in task.serializers

--- task/serializers.py
+++ task/serializers.py
@@ -8,15 +8,14 @@ class TaskCreateSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Task
-        fields = ('title', 'description',
-                  'status', 'assigned_user')
+        fields = ('title', 'description', 'status',)
         
 class TaskReadSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField('get_user_full_name')
     
     class Meta:
         model = Task
-        fields = ('title', 'status', 'user')
+        fields = ('id', 'title', 'status', 'user')
         
     def get_user_full_name(self, obj):
         return obj.assigned_user.get_full_name()
