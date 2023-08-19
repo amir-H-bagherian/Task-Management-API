0cc741b78a99eaf258c02e3af76b01db13d579c7
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-19 22:09:39 +0330

    feat: Email Configs added to settings.py file

--- core/settings.py
+++ core/settings.py
@@ -139,3 +139,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
