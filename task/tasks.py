ed1f5f7126104e8d511a75202d7ab2082f7b40e7
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-21 14:25:14 +0330

    perf: send_email_to_user moved from utils.py to tasks.py in order to handle sending emails by celery

--- task/tasks.py
+++ task/tasks.py
@@ -1,7 +1,15 @@
+import os
+from celery import Celery, shared_task
 from django.core.mail import send_mail
 from core.settings import EMAIL_HOST_USER
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+app = Celery('tasks')
+app.config_from_object("task.celeryconfig")
+app.autodiscover_tasks()
 
+
+@shared_task
 def send_email_to_user(task_title, email):
     SUBJECT = "Status Changed"
     MESSAGE = f"Task <{task_title}> is now Completed"
@@ -13,5 +21,6 @@ def send_email_to_user(task_title, email):
             recipient_list=[email],
             fail_silently=False,
         )
-    except:
-        pass
+        return {"msg": "Sent successfully"}
+    except Exception as e:
+        return {"msg": e.args}
