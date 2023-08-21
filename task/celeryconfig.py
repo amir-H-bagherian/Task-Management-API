83642be8f6693e6fe22125573151ce16c3f54d7d
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-21 14:29:33 +0330

    chore: celery config added to task.celeryconfig.py and settings.py. could not link task.tasks to configs in settings. debug needed

---
+++ task/celeryconfig.py
@@ -0,0 +1,8 @@
+broker_url = "redis://127.0.0.1:6379"
+result_backend = "redis://127.0.0.1:6379"
+
+task_serializer = "json"
+result_serializer = "json"
+accept_content = ["json"]
+timezone = "Asia/Tehran"
+enable_utc = True
